107c5f35aebe9071ddfc0dd82b75857c197a9068
cccxxmo <55021745+cccxxmo@users.noreply.github.com>
2019-09-16 11:38:19 +0800

    Add files via upload

---
+++ Move.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour {
+    public float speed;
+    float rx, ry;
+
+    // Use this for initialization
+    void Start () {
+    	
+        rx = Random.Range(-90, 90);
+        ry = Random.Range(-90, 90);
+    }	
+
+	// Update is called once per frame
+	void Update () {
+        Vector3 raxis = new Vector3(rx, ry, 0);
+        this.transform.RotateAround(this.transform.parent.position, raxis, speed * Time.deltaTime);
+    }
+
+}
+
